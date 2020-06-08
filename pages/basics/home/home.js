f7192ce04d40ec2d49b4dab78b5a40dfe902df4a
xuzhoulin <danlyxzl@163.com>
2020-06-08 16:42:20 +0800

    .

--- pages/basics/home/home.js
+++ pages/basics/home/home.js
@@ -9,8 +9,14 @@ Component({
     TabCur: 0,
     scrollLeft: 0,
     tabNav: ['定制比赛', '我的比赛'],
+    createBackground: "/images/createFive.png",
   },
-  
+  onLoad:function(){
+    let base64 = wx.getFileSystemManager().readFileSync(this.data.createBackground, 'base64');
+    this.setData({
+      createBackground:'data:image/png;base64,' + base64,
+    })
+  },
   methods: {
     //tab event事件捕获
     tabSelect(e) {
