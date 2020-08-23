342713c2e61e3cc9eb9e5492639d0a67836d5bed
saiyan <sabersama1208@163.com>
2020-08-23 16:35:34 +0800

    .

--- app.js
+++ app.js
@@ -85,10 +85,10 @@ App({
     imageUrl:'http://images.esale.chinacarabao.com',
 
     // websocketUrl: 'wss://ksaiwss.nong12.com',
-    // url: 'https://ksai.nong12.com'
+    url: 'https://ksai.nong12.com',
 
      websocketUrl: 'ws://localhost:8080',
-     url: 'http://localhost:8080'
+     // url: 'http://localhost:8080'
 
     // websocketUrl: 'ws://118.114.77.102:9258',
     // url:'http://118.114.77.102:9258'
