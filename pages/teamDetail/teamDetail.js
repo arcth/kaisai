ad98373475ec865584da0275ad779d7b3122c768
saiyan <sabersama1208@163.com>
2020-10-08 20:40:45 +0800

    .

---
+++ pages/teamDetail/teamDetail.js
@@ -0,0 +1,16 @@
+const util = require('../../utils/util.js');
+const app = getApp()
+const api = app.globalData.api
+Page({
+	data:{
+		CustomBar: app.globalData.CustomBar,
+		isLogin:null,
+		needLoginShow:false,
+		userInfo:{}
+	},
+	onLoad:function(options){
+		
+    },
+	
+	
+})
